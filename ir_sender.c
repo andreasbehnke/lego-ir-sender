7f0ad12ec3b500eb2ea824d2aaa3d6da10390654
Andreas Behnke <ab@andreasbehnke.com>
2016-02-06 17:30:15 +0100

    Do not try to abstract hardware for timer configuration, code is unreadable

--- ir_sender.c
+++ ir_sender.c
@@ -1,20 +1,92 @@
-
-#include "include/hardware.h"
+#include <avr/interrupt.h>
+#include <util/atomic.h>
 #include "include/ir_sender.h"
 
-inline void ir_sender_init() {
-    IR_INIT_PIN;
-    IR_INIT_TCCRxA;
-    IR_INIT_TCCRxB;
-    IR_INIT_OCRxA;
-    IR_CLEAR_PIN;
+#define IR_STATE_STOPPED 0
+#define IR_STATE_PULSE 1
+#define IR_STATE_PAUSE 2
+
+volatile uint8_t ir_state = IR_STATE_STOPPED;
+volatile uint8_t ir_pulse_count;
+volatile uint8_t ir_pause_count;
+
+inline void ir_clear_pin() {
+    OC0A_PORT &= ~(_BV(OC0A_BIT));
 }
 
-void ir_pulse(uint8_t pulse_count) {
-    IR_CLEAR_PIN;
-    IR_START_TIMER;
+inline void ir_enable_pwm_output() {
+    TCCR0A |= _BV(COM0A0);
+}
+
+inline void ir_disable_pwm_output() {
+    TCCR0A &= ~(_BV(COM0A0));
+}
+
+inline void ir_sender_init() {
+    TCNT0 = 0;
+    // PWM output pin
+    OC0A_DDR |= _BV(OC0A_BIT);
+    ir_clear_pin();
+    // Clear Timer on Compare Match (CTC) Mode
+    TCCR0A = _BV(WGM01);
+    // do not use prescaler
+    TCCR0B = _BV(CS00);
+    // match value
+    OCR0A = IR_DUTY_CYCLE;
+    // enable interrupt on compare match
+    TIMSK0 |= _BV(OCIE0A);
+
+    // debug LED
+    DDRB |= _BV(PINB0);
+}
+
+void ir_pulse(uint8_t pulse_count, uint8_t pause_count) {
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        // interrupt is triggered for every half pulse,
+        // multiply pulse count by 2
+        ir_clear_pin();
+        ir_enable_pwm_output();
+        ir_pulse_count = pulse_count * 2;
+        ir_pause_count = pause_count * 2;
+        ir_state = IR_STATE_PULSE;
+    }
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+    switch (ir_state) {
+
+        case IR_STATE_PULSE:
+            if (ir_pulse_count == 0) {
+
+                // toggle debug led
+                PORTB ^= _BV(PB0);
+
+                ir_disable_pwm_output();
+                ir_clear_pin();
+                // all pulse send, now wait ir_pause_count cycles
+                ir_state = IR_STATE_PAUSE;
+            } else {
+                ir_pulse_count--;
+            }
+            break;
+
+        case IR_STATE_PAUSE:
+            if (ir_pause_count == 0) {
+                // end of pause, signal everyone that we are ready
+                // for next bit
+                ir_state = IR_STATE_STOPPED;
+            } else {
+                ir_pulse_count--;
+            }
+            break;
+
+        default:
+            ir_state = IR_STATE_STOPPED;
+            break;
+    }
 }
 
 void ir_wait() {
-
+    while(ir_state != IR_STATE_STOPPED) {}
 }
--- main.c
+++ main.c
@@ -4,11 +4,14 @@
  *  Created on: 09.01.2016
  *      Author: andreasbehnke
  */
+#include <avr/interrupt.h>
 #include "include/ir_sender.h"
 
 int main() {
     ir_sender_init();
-    ir_pulse(6);
+    sei();
     while (1) {
+        ir_pulse(100, 10);
+        ir_wait();
     }
 }
