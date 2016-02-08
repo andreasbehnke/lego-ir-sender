9475a10a4a8ed28cde583c226a3f0c28b50c6141
Andreas Behnke <ab@andreasbehnke.com>
2016-02-08 21:55:16 +0100

    filter unsued bits of nibble byte in lrc function

--- power_functions.c
+++ power_functions.c
@@ -11,15 +11,15 @@
 // calculate longitudinal redundancy check and return complete command
 static uint16_t calculate_lrc(uint8_t nibble1, uint8_t nibble2, uint8_t nibble3) {
     uint8_t lrc = 0x0F ^ nibble1 ^ nibble2 ^ nibble3;
-    return nibble1 << 12 | nibble2 << 8 | nibble3 << 4 | (lrc & 0x0F );
+    return nibble1 << 12 | (nibble2 & 0x0F) << 8 | (nibble3 & 0x0F) << 4 | (lrc & 0x0F );
 }
 
 extern void pf_combo_pwm_mode(uint8_t channel, uint8_t output_a, uint8_t output_b) {
     ir_send_message(
             calculate_lrc(
                     (channel & 0x03) | 0b0100, // a 1 C C
-                    output_b & 0x0F,           // b b b b
-                    output_a & 0x0F            // a a a a
+                    output_b,                  // b b b b
+                    output_a                   // a a a a
                 )
             );
 }
