4a8dfcabfca7b9f0e900d5c35f1ec2d7cefcc0c3
Andreas Behnke <ab@andreasbehnke.com>
2016-03-01 19:55:02 +0100

    declaration of command input API

---
+++ include/command_input.h
@@ -0,0 +1,25 @@
+/*
+ * command_input.h
+ *
+ *  Created on: 29.02.2016
+ *      Author: andreasbehnke
+ */
+
+#ifndef INCLUDE_COMMAND_INPUT_H_
+#define INCLUDE_COMMAND_INPUT_H_
+
+#include <stdbool.h>
+
+struct command {
+        uint8_t command_a;
+        uint8_t command_b;
+        bool is_halt;
+        bool is_break;
+        bool has_command_changed;
+};
+
+extern void init_command();
+
+extern struct command read_command(uint8_t channel);
+
+#endif /* INCLUDE_COMMAND_INPUT_H_ */
